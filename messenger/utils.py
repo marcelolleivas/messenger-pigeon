20b2777018cbc2d3263931ac34671b78f7be6495
marcelolleivas <marcelo.lleivas@gmail.com>
2022-10-18 20:12:51 -0300

    safety adjustments

--- messenger/utils.py
+++ messenger/utils.py
@@ -3,6 +3,6 @@ import jinja2
 
 def template_render(search_path, template_file):
     template_loader = jinja2.FileSystemLoader(searchpath=search_path)
-    template_env = jinja2.Environment(loader=template_loader)
+    template_env = jinja2.Environment(loader=template_loader, autoescape=True)
     template = template_env.get_template(template_file)
     return template.render()
