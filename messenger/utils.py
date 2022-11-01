076625f6f66194d6565a61f840c8b9c0e0280151
marcelolleivas <marcelo.lleivas@gmail.com>
2022-11-01 16:04:26 -0300

    edit render template to use a default one

--- messenger/utils.py
+++ messenger/utils.py
@@ -1,8 +1,17 @@
 import jinja2
+from jinja2 import select_autoescape
+
+SEARCH_PATH = "../templates"
+TEMPLATE_FILE = "simple_email.html"
 
 
-def template_render(search_path, template_file):
-    template_loader = jinja2.FileSystemLoader(searchpath=search_path)
-    template_env = jinja2.Environment(loader=template_loader, autoescape=True)
-    template = template_env.get_template(template_file)
-    return template.render()
+def template_render(title, message):
+    template_loader = jinja2.FileSystemLoader(searchpath=SEARCH_PATH)
+    template_env = jinja2.Environment(
+        loader=template_loader, autoescape=select_autoescape(["html"])
+    )
+    template = template_env.get_template(TEMPLATE_FILE)
+
+    data = {"title": title, "message": message}
+
+    return template.render(data)
